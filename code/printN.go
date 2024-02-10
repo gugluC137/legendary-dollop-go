30d8039b31298be30886bd6d464a6c0a120787cf
gugluC137 <behera.biswa5398@gmail.com>
2024-02-11 00:18:24 +0530

    initial setup

---
+++ code/printN.go
@@ -0,0 +1,7 @@
+package code
+
+import "fmt"
+
+func PrintN(num int) {
+	fmt.Printf("the number is %v", num)
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gugluC137/legendary-dollop-go/code"
+)
+
+func main() {
+	fmt.Println("Hello, World!!")
+
+	code.PrintN(3)
+}
